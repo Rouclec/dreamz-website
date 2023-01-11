d16749db28369088e1eadbfa2762f306aa572bca
rouclec <senatorasonganyi97@gmail.com>
2023-01-11 15:54:20 +0100

    updated whyUs

--- components/WhyUs.js
+++ components/WhyUs.js
@@ -7,12 +7,12 @@ export default function WhyUs() {
         <div class="row g-5 align-items-center">
           <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
             <p class="d-inline-block border rounded text-primary fw-semi-bold py-1 px-3">
-              Why use a travel agent
+              Why DREAMZ?
             </p>
             <h1 class="display-5 mb-4">Few Reasons Why People Choosing Us!</h1>
             <p class="mb-4">
-              Wondering why you need a business or travel assistant? Here are
-              some reasons to help you
+              Wondering why you need a business or travel or logistic assistant?
+              Here are some reasons to help you
             </p>
             <a class="btn btn-primary py-3 px-5" href="">
               Explore More
@@ -41,9 +41,10 @@ export default function WhyUs() {
                       <i class="fa fa-check fa-3x text-primary mb-3"></i>
                       <h4 class="mb-3">The convenience you need</h4>
                       <p class="mb-3">
-                        We are here to provide you suggestions on exclusive
-                        places which will make your travel experience fun! Let
-                        us bother about your travel procedures
+                        Our logistic services enable you send and receive
+                        packages internationally with lesser cost and more
+                        convinience. We help track your goods in transit and
+                        make sure they arrive on time
                       </p>
                       <a class="fw-semi-bold" href="">
                         Read More <i class="fa fa-arrow-right ms-1"></i>
@@ -57,11 +58,11 @@ export default function WhyUs() {
                   <div class="col-12 wow fadeIn" data-wow-delay="0.3s">
                     <div class="feature-box border rounded p-4">
                       <i class="fa fa-check fa-3x text-primary mb-3"></i>
-                      <h4 class="mb-3">Destination Knowledge</h4>
+                      <h4 class="mb-3">Business management</h4>
                       <p class="mb-3">
-                        At DREAMZ, we uncover new destinations and are in the
-                        lookout for best trips and travel recommendations and
-                        residences
+                        We at DREAMZ focus on managing your business needs. We
+                        take care of your buying and delivery of quality assured
+                        goods from Dubia and even handle your damages!
                       </p>
                       <a class="fw-semi-bold" href="">
                         Read More <i class="fa fa-arrow-right ms-1"></i>
@@ -73,8 +74,8 @@ export default function WhyUs() {
                       <i class="fa fa-check fa-3x text-primary mb-3"></i>
                       <h4 class="mb-3">Cost saving</h4>
                       <p class="mb-3">
-                        Our expertise in travels will help you spend your money
-                        wisely and on relevant stuffs
+                        Our expertise in travels, logistics and business will
+                        help you spend your money wisely and on relevant stuffs
                       </p>
                       <a class="fw-semi-bold" href="">
                         Read More <i class="fa fa-arrow-right ms-1"></i>
